73243e33a48b49af7902abbcefee12497b75e4d0
songwun <92703380+songwun@users.noreply.github.com>
2022-01-02 13:47:32 +0800

    Add files via upload

---
+++ Class13.java
@@ -0,0 +1,38 @@
+package Java11;
+interface Show_ans { 
+	public void show(); 
+} 
+interface Math extends Show_ans { 
+	public void add(int a,int b); // 計算 a+b 
+	public void sub(int a,int b); // 計算 a-b 
+	public void mul(int a,int b); // 計算 a*b 
+	public void div(int a,int b); // 計算 a/b 
+}
+class Compute implements Math { 
+	public int ans; 
+	public void add(int a,int b) { 
+		ans=a+b; 
+	} 
+	public void sub(int a,int b) {
+		ans=a-b; 
+	} 
+	public void mul(int a,int b) { 
+		ans=a*b;
+	} 
+	public void div(int a,int b) { 
+		ans=a/b; 
+	} 
+	public void show() { 
+		System.out.println("ans="+ans); 
+	} 
+}
+public class Class13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Compute cmp=new Compute(); 
+		 cmp.mul(3,5); // 計算 3*5 
+		 cmp.show(); // 此行會回應"ans=15"字串 
+	}
+
+}
---
+++ Class14.java
@@ -0,0 +1,35 @@
+package Java11;
+interface iVolume { 
+	public void showData(); // 顯示球體的資料
+	public double vol(); // 計算球體積
+} 
+abstract class CSphere implements iVolume { 
+	final double PI=3.14; 
+	protected int x; 
+	protected int y; 
+	protected int radius; // radius 必須放到 CCircle 的父類別裡
+	public void showData() { 
+		System.out.println("球心:("+x+","+y+")"); 
+		System.out.println("半徑:"+radius); 
+		System.out.println("球體積:"+vol()); 
+	} 
+	public double vol() { 
+		return ((4.0/3)*PI*radius*radius*radius); 
+	} 
+} 
+class CCircle extends CSphere { 
+	public CCircle(int a,int b,int r) { 
+		x=a; 
+		y=b; 
+		radius=r; 
+	} 
+}
+public class Class14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CCircle cir=new CCircle(8,6,2); 
+		cir.showData(); 
+	}
+
+}
